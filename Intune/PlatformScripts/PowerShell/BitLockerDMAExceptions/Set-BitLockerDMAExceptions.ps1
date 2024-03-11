b91c73cb41be0f75ef5974d3ff3e2c7470f5450e
ennnbeee <nickjbenton@gmail.com>
2024-03-11 21:06:47 +0000

    DMA Exception Script

--- Intune/PlatformScripts/PowerShell/BitLockerDMAExceptions/Set-BitLockerDMAExceptions.ps1
+++ Intune/PlatformScripts/PowerShell/BitLockerDMAExceptions/Set-BitLockerDMAExceptions.ps1
@@ -6,22 +6,27 @@
 $regPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\DmaSecurity\AllowedBuses'
 $dmaDevices = @()
 
+$devicesLenovo = @('20UACTO1WW', '20KG0005AU')
+$devicesDell = @('Latitude 7320','Latitude 5320')
+$devicesHP = @('HP EliteBook 8540p')
+
 Try {
 
     $deviceModel = (Get-WmiObject -Class:Win32_ComputerSystem).Model
 
-    if ($deviceModel -in 'Latitude 1', 'Latitude 2') {
+    if ($deviceModel -in $devicesDell) {
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) 300 Series Chipset Family LPC Controller (HM370) - A30D'; value = 'PCI\VEN_8086&DEV_A30D'}
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) PCI Express Root Port #13 - A334'; value = 'PCI\VEN_8086&DEV_A334'}
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) PCI Express Root Port #16 - A337'; value = 'PCI\VEN_8086&DEV_A337'}
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) PCI Express Root Port #20 - A343'; value = 'PCI\VEN_8086&DEV_A343'}
+    }
+    elseif ($deviceModel -in $devicesLenovo) {
+        $dmaDevices += [pscustomobject]@{name = 'PCI Express Downstream Switch Port'; value = 'PCI\VEN_8086&DEV_15C0'}
+    }
+    elseif ($deviceModel -in $devicesHP ){
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) PCI Express Root Port #9 - A330'; value = 'PCI\VEN_8086&DEV_A330'}
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) Xeon(R) E3 - 1200/1500 v5/6th Gen Intel(R) Core(TM) PCIe Controller (x16) - 1901'; value = 'PCI\VEN_8086&DEV_1901'}
         $dmaDevices += [pscustomobject]@{name = 'Intel(R) PCI Express Root Port #15 - A336'; value = 'PCI\VEN_8086&DEV_A336'}
-
-    }
-    elseif ($deviceModel -in 'Lenovo 1', 'Lenovo 2') {
-        $dmaDevices += [pscustomobject]@{name = 'PCI Express Downstream Switch Port'; value = 'PCI\VEN_8086&DEV_15C0'}
     }
 
     foreach ($dmaDevice in $dmaDevices) {
