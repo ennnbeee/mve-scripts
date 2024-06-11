b40ceb49b3d0383bda3e8e1f77c3a884ddd443b3
ennnbeee <nickjbenton@gmail.com>
2024-06-11 15:23:36 +0100

    CIS Updates

--- Intune/PlatformScripts/PowerShell/CIS/CIS (BL) BitLocker TPMandPIN - Windows 11 Intune 3.0.1.ps1
+++ Intune/PlatformScripts/PowerShell/CIS/CIS (BL) BitLocker TPMandPIN - Windows 11 Intune 3.0.1.ps1
@@ -1,30 +1,32 @@
 Try {
     $osVolume = Get-BitLockerVolume | Where-Object { $_.VolumeType -eq 'OperatingSystem' }
-    if ($osVolume.VolumeStatus -eq 'FullyDecrypted') {
 
-        if ($osVolume.KeyProtector.KeyProtectorType -contains 'TpmPin') {
-            $osVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'TpmPin' } | ForEach-Object {
-                Remove-BitLockerKeyProtector -MountPoint $osVolume.MountPoint -KeyProtectorId $_.KeyProtectorId
-            }
+    # Detects and removes existing TpmPin key protectors as there can only be one
+    if ($osVolume.KeyProtector.KeyProtectorType -contains 'TpmPin') {
+        $osVolume.KeyProtector | Where-Object { $_.KeyProtectorType -eq 'TpmPin' } | ForEach-Object {
+            Remove-BitLockerKeyProtector -MountPoint $osVolume.MountPoint -KeyProtectorId $_.KeyProtectorId
         }
-
-        $deviceSerial = (((Get-WmiObject -Class win32_bios).Serialnumber).ToUpper() -replace '[^a-zA-Z0-9]', '')
-        # Upper Case letters, removes spaces and special characters
-        If ($deviceSerial.length -gt 14) {
-            $deviceSerial = $deviceSerial.Substring(0, 14) # Reduce to 14 characters if longer
-        }
-
-        $devicePIN = ConvertTo-SecureString $deviceSerial -AsPlainText -Force
-        Enable-BitLocker -MountPoint $osVolume.MountPoint -Pin $devicePIN -TpmAndPinProtector -ErrorAction SilentlyContinue | Out-Null
-        ((Get-BitLockerVolume).KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' }).KeyProtectorId | ForEach-Object {
-            BackupToAAD-BitLockerKeyProtector -MountPoint $osVolume.MountPoint -KeyProtectorId $_
-        }
-        Exit 0
     }
-    else {
-        Write-Output 'Device Encrypted.'
-        Exit 0
+
+    # Sets a recovery password key protector if one doesn't exist, needed for TpmPin key protector
+    if ($osVolume.KeyProtector.KeyProtectorType -notcontains 'RecoveryPassword') {
+        Enable-BitLocker -MountPoint $osVolume.MountPoint -RecoveryPasswordProtector
     }
+
+    # Configures the PIN and Enables BitLocker using the TpmPin key protector
+    $deviceSerial = (((Get-WmiObject -Class win32_bios).Serialnumber).ToUpper() -replace '[^a-zA-Z0-9]', '')
+    If ($deviceSerial.length -gt 14) {
+        $deviceSerial = $deviceSerial.Substring(0, 14) # Reduce to 14 characters if longer
+    }
+
+    $devicePIN = ConvertTo-SecureString $deviceSerial -AsPlainText -Force
+    Enable-BitLocker -MountPoint $osVolume.MountPoint -Pin $devicePIN -TpmAndPinProtector -ErrorAction SilentlyContinue | Out-Null
+
+    # Gets the recovery key and escrows to Entra
+    (Get-BitLockerVolume).KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } | ForEach-Object {
+        BackupToAAD-BitLockerKeyProtector -MountPoint $osVolume.MountPoint -KeyProtectorId $_.KeyProtectorId
+    }
+    Exit 0
 }
 Catch {
     $ErrorMessage = $_.Exception.Message
