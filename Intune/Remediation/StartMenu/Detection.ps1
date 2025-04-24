19b328d740bdd3ae19b8f17f42b1f81bbc542c51
ennnbeee <nickjbenton@gmail.com>
2025-04-24 11:16:02 +0100

    WUCP Remediations

--- Intune/Remediation/StartMenu/Detection.ps1
+++ Intune/Remediation/StartMenu/Detection.ps1
@@ -14,7 +14,7 @@ Start-Transcript -Path $transcriptPath\$transcriptName -Append
 # Start Menu settings
 [PsObject[]]$regKeysStartMenu = @()
 # Start Menu keys and values
-$regKeysStartMenu += [PsObject]@{ Name = 'TaskbarAl'; path = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\'; value = 0; type = 'DWord' }
+$regKeysStartMenu += [PsObject]@{ Name = 'TaskbarAl'; path = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\'; value = 0; type = 'DWord' } # 0 is left, 1 is right
 
 
 foreach ($setting in $regKeysStartMenu) {
